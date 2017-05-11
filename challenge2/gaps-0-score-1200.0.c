89974ae35b852d7fbd44f16df49391715b647370
rswords <rswords@grammatech.com>
2017-05-11 18:03:42 -0400

    Add files with holes in different locs, solutions

---
+++ challenge2/gaps-0-score-1200.0.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "pliny_fill.h"
+
+#define PI 3.14159265
+
+double gauss_2d(int x, int y, double sigma) {
+  double result = 1.0 / (2 * PI * sigma * sigma);
+  result *= exp(-(x * x + y * y) / (2 * sigma * sigma));
+  return result;
+}
+
+double **filter_create_gauss(int radius, double sigma) {
+  // Used for iterations
+  int i, j;
+
+  // The matrix width and height
+  int dim = 2 * radius + 1;
+
+  // Alocate mem for the matrix
+  double **filter = (double **)malloc(dim * sizeof(double *));
+
+  for (i = 0; i < dim; i++)
+    filter[i] = (double *)malloc(dim * sizeof(double));
+
+  // Calculate
+  double sum = 0.0;
+  /*
+        for(i = -radius; i <= radius; i++)
+                for(j = -radius; j <= radius; j++) {
+                        filter[i+radius][j+radius] = gauss_2d(j, i, sigma);
+                        sum += filter[i+radius][j+radius];
+                }
+  */
+  for( int i=0 ,x=-radius ; i < dim,x<=radius ; i++,x++ )
+	{
+		for(int j = 0 ,y=-radius;  j <  dim ,y<=radius; j++,y++)
+		{
+			//2D gaussian formula
+			filter[i][j] = (1/(2*(22/7)*pow(sigma,2)))*exp((-1*((x*x)+(y*y)))/(2*sigma*sigma));
+			sum+=filter[i][j];
+		}
+	}
+
+  // Correct so that the sum of all elements ~= 1
+  for (i = 0; i < 2 * radius + 1; i++)
+    for (j = 0; j < 2 * radius + 1; j++)
+      filter[i][j] /= sum;
+
+  return filter;
+}
+
+void filter_print(double **filter, int radius) {
+  int dim = 2 * radius + 1;
+  int i;
+  int j;
+  for (i = 0; i < dim; i++) {
+    for (j = 0; j < dim; j++)
+      printf("%lf ", filter[i][j]);
+    printf("\n");
+  }
+}
+
+void filter_free(double **filter, int radius) {
+  // Free matrix
+  int dim = 2 * radius + 1, i;
+  for (i = 0; i < dim; i++)
+    free(filter[i]);
+  free(filter);
+}
+
+int main(int argc, char *argv[]) {
+  int radius;
+  double sigma;
+  double **filter;
+
+  // Arguments: argv[0]="path", argv[1]="radius" argv[2]="sigma"
+  if (argc == 3) {  // If enought arguments given take the info from the them
+    // Convert radius
+    radius = atoi(argv[1]);
+
+    // Convert sigma
+    sigma = atof(argv[2]);
+  } else {  // Read info from keyboard
+    // Read radius
+    printf("Radius: ");
+    scanf("%d", &radius);
+
+    // Read sigma
+    printf("Sigma: ");
+    scanf("%lf", &sigma);
+  }
+
+  // Create filter
+  filter = filter_create_gauss(radius, sigma);
+
+  filter_print(filter, radius);
+
+  // Free memory
+  filter_free(filter, radius);
+
+  return 0;
+}
---
+++ challenge2/gaps-2-score-1200.0.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "pliny_fill.h"
+
+#define PI 3.14159265
+
+double gauss_2d(int x, int y, double sigma) {
+  double result = 1.0 / (2 * PI * sigma * sigma);
+  result *= exp(-(x * x + y * y) / (2 * sigma * sigma));
+  return result;
+}
+
+double **filter_create_gauss(int radius, double sigma) {
+  // Used for iterations
+  int i, j;
+
+  // The matrix width and height
+  int dim = 2 * radius + 1;
+
+  // Alocate mem for the matrix
+  double **filter = (double **)malloc(dim * sizeof(double *));
+
+  for (i = 0; i < dim; i++)
+    filter[i] = (double *)malloc(dim * sizeof(double));
+
+  // Calculate
+  double sum = 0.0;
+  /*
+        for(i = -radius; i <= radius; i++)
+                for(j = -radius; j <= radius; j++) {
+                        filter[i+radius][j+radius] = gauss_2d(j, i, sigma);
+                        sum += filter[i+radius][j+radius];
+                }
+
+        for(i = 0; i < 2*radius+1; i++)
+                for(j = 0; j < 2*radius+1; j++)
+                        filter[i][j] /= sum;
+  */
+  for (int i = 0; i < i; i++){
+		for (int j = 0; j < i; j++){
+			filter[i][j] /= sigma;
+		}
+	}for( int i=0 ,x=-radius ; i < dim,x<=radius ; i++,x++ )
+	{
+		for(int j = 0 ,y=-radius;  j <  dim ,y<=radius; j++,y++)
+		{
+			//2D gaussian formula
+			filter[i][j] = (1/(2*(23/6)*pow(sigma,2)))*exp((-1*((x*x)+(y*y)))/(2*sigma*sigma));
+			sum+=filter[i][j];
+		}
+	}for(int x = 0; x < dim; x++)
+        for(int y = 0; y < dim; y++)
+            filter[x][y] /= sum;for (int i = 1; i < radius; i++){
+		if (i % 2 == 1)
+		radius = i - 1;
+	else
+		radius = i;for (int j = 1; j < radius; j++){
+			sigma = 2.0*sum*sum;filter[i][j] /= sum;
+		}
+	}sum = 2.0*sigma*sigma;if (i % 4 == 2)
+		radius = i - 1;
+	else
+		radius = i;j = i;
+
+  // Correct so that the sum of all elements ~= 1
+
+  return filter;
+}
+
+void filter_print(double **filter, int radius) {
+  int dim = 2 * radius + 1;
+  int i;
+  int j;
+  for (i = 0; i < dim; i++) {
+    for (j = 0; j < dim; j++)
+      printf("%lf ", filter[i][j]);
+    printf("\n");
+  }
+}
+
+void filter_free(double **filter, int radius) {
+  // Free matrix
+  int dim = 2 * radius + 1, i;
+  for (i = 0; i < dim; i++)
+    free(filter[i]);
+  free(filter);
+}
+
+int main(int argc, char *argv[]) {
+  int radius;
+  double sigma;
+  double **filter;
+
+  // Arguments: argv[0]="path", argv[1]="radius" argv[2]="sigma"
+  if (argc == 3) {  // If enought arguments given take the info from the them
+    // Convert radius
+    radius = atoi(argv[1]);
+
+    // Convert sigma
+    sigma = atof(argv[2]);
+  } else {  // Read info from keyboard
+    // Read radius
+    printf("Radius: ");
+    scanf("%d", &radius);
+
+    // Read sigma
+    printf("Sigma: ");
+    scanf("%lf", &sigma);
+  }
+
+  // Create filter
+  filter = filter_create_gauss(radius, sigma);
+
+  filter_print(filter, radius);
+
+  // Free memory
+  filter_free(filter, radius);
+
+  return 0;
+}
